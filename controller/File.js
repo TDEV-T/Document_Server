1224a930512a4a208b9c8e76fa83b1de4235f7a3
ExtraCB <ktgz14211771329@gmail.com>
2023-03-24 15:22:05 +0700

    api new form 75%

--- controller/File.js
+++ controller/File.js
@@ -3,31 +3,50 @@ const { db } = require("../config/db");
 exports.create_document = async (req, res) => {
   try {
     const data = req.body;
-    let fileAll = "";
 
-    for (let x = 0; x < req.files.length; x++) {
-      fileAll += req.files[x].filename;
-      if (x != req.files.length - 1) {
-        fileAll += ",";
-      }
-    }
+    console.log(data);
+
+    let idReport;
+    // let fileAll = "";
+
+    // for (let x = 0; x < req.files.length; x++) {
+    //   fileAll += req.files[x].filename;
+    //   if (x != req.files.length - 1) {
+    //     fileAll += ",";
+    //   }
+    // }
 
     db.query(
-      "INSERT INTO file(header_file,location_file,start_file,end_file,content_file,file_file,own_file) VALUES (?,?,?,?,?,?,?)",
+      "INSERT INTO `reports`( `start_re`, `activity_re`, `project_re`, `another_re`, `dev_re`, `manage_re`, `plans_re`, `college_re`, `group_re`, `work_re`, `dept_re`, `location_re`, `name_re`, `benefit_re`, `comment_re`, `own_re`, `img_re`, `result_re`) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)",
       [
-        data.header,
-        data.location,
-        data.start,
-        data.end,
-        data.content,
-        fileAll,
-        data.own,
+        data.start_re,
+        data.activity_re,
+        data.project_re,
+        data.another_re,
+        data.dev_re,
+        data.manage_re,
+        data.plans_re,
+        data.college_re,
+        data.group_re,
+        data.work_re,
+        data.dept_re,
+        data.location_re,
+        data.name_re,
+        data.benefit_re,
+        data.comment_re,
+        data.own_re,
+        "",
+        data.result_re,
       ],
       (err, result) => {
         if (err) {
           res.json({ status: "error", message: err });
         } else {
-          res.json({ status: "success", message: "Success Create Document" });
+          idReport = result.insertId;
+
+          for (let i = 0; i < data.part.length; i++) {
+            console.log(data.part[i]);
+          }
         }
       }
     );
