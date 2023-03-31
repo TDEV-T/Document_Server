dfabfcd979ae4ac552dadd9b9da6565f5b2791f7
ExtraCB <ktgz14211771329@gmail.com>
2023-03-31 16:14:56 +0700

    success new form

--- controller/File.js
+++ controller/File.js
@@ -7,17 +7,18 @@ exports.create_document = async (req, res) => {
     console.log(data);
 
     let idReport;
-    // let fileAll = "";
 
-    // for (let x = 0; x < req.files.length; x++) {
-    //   fileAll += req.files[x].filename;
-    //   if (x != req.files.length - 1) {
-    //     fileAll += ",";
-    //   }
-    // }
+    let fileAll = "";
+
+    for (let x = 0; x < req.files.length; x++) {
+      fileAll += req.files[x].filename;
+      if (x != req.files.length - 1) {
+        fileAll += ",";
+      }
+    }
 
     db.query(
-      "INSERT INTO `reports`( `start_re`, `activity_re`, `project_re`, `another_re`, `dev_re`, `manage_re`, `plans_re`, `college_re`, `group_re`, `work_re`, `dept_re`, `location_re`, `name_re`, `benefit_re`, `comment_re`, `own_re`, `img_re`, `result_re`) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)",
+      "INSERT INTO `reports` (`start_re`, `activity_re`, `project_re`, `another_re`, `dev_re`, `manage_re`, `plans_re`, `technical_re`, `college_re`, `group_re`, `work_re`, `dept_re`, `location_re`, `name_re`, `content_re`, `benefit_re`, `comment_re`, `own_re`, `img_re`, `result_re`, `statusmade_re`, `namemade_re`, `deptmade_re`) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)",
       [
         data.start_re,
         data.activity_re,
@@ -26,17 +27,22 @@ exports.create_document = async (req, res) => {
         data.dev_re,
         data.manage_re,
         data.plans_re,
+        data.technical_re,
         data.college_re,
         data.group_re,
         data.work_re,
         data.dept_re,
         data.location_re,
         data.name_re,
+        data.content_re,
         data.benefit_re,
         data.comment_re,
         data.own_re,
-        "",
+        fileAll,
         data.result_re,
+        data.statusmade_re,
+        data.namemade_re,
+        data.deptmade_re,
       ],
       (err, result) => {
         if (err) {
@@ -53,11 +59,11 @@ exports.create_document = async (req, res) => {
             const obj = resultPart[i];
             const query = `INSERT INTO participant (re_pa, name_pa,value_pa) VALUES ('${idReport}','${obj.name}','${obj.count}')`;
             db.query(query, (err, result) => {
-              if(err) throw err;
+              if (err) throw err;
             });
           }
 
-          res.json({status:"success",message:"Create Success !"});
+          res.json({ status: "success", message: "Create Success !" });
         }
       }
     );
@@ -70,8 +76,27 @@ exports.create_document = async (req, res) => {
 exports.get_document = async (req, res) => {
   try {
     db.query(
-      "SELECT `id_file`,`header_file`,`content_file`,`file_file`,`own_file`,`timestamp_file`, DATE_FORMAT(start_file, '%Y/%m/%d') as start_file,DATE_FORMAT(end_file, '%Y/%m/%d') as end_file FROM file WHERE own_file = ?",
+      "SELECT * FROM reports WHERE own_re = ? ",
       [req.body.userid],
+      (err, result) => {
+        if (err) {
+          res.json({ status: "error", message: err });
+        } else {
+          res.send(result);
+        }
+      }
+    );
+  } catch (err) {
+    console.log(err);
+    res.status(401).send("Server Error");
+  }
+};
+
+exports.get_part = async (req, res) => {
+  try {
+    db.query(
+      "SELECT * FROM participant WHERE re_pa  = ?",
+      [req.body.id_re],
       (err, result) => {
         if (err) {
           res.json({ status: "error", message: err });
--- routes/File.js
+++ routes/File.js
@@ -1,4 +1,8 @@
-const { create_document, get_document } = require("../controller/File");
+const {
+  create_document,
+  get_document,
+  get_part,
+} = require("../controller/File");
 const express = require("express");
 const route = express.Router();
 
@@ -8,5 +12,6 @@ const { upload } = require("./../middleware/upload");
 
 route.post("/create_document", auth, upload.array("files"), create_document);
 route.post("/get_document", auth, get_document);
+route.post("/get_part", auth, get_part);
 
 module.exports = route;
